17a4281afcd3a1cc3ec37b87a28da6cf44c06ad4
Fagner Pimentel <fagnerpimentel@gmail.com>
2023-05-15 15:04:00 +0000

    add subscriber

--- src/arm_control.py
+++ src/arm_control.py
@@ -14,6 +14,9 @@ class arm_control():
  
     def __init__(self):
         self.time_to_wait = 5.0
+
+        self sub = rospy.Subscriber("chatter", String, callback);
+
         self.pub = rospy.Publisher("/cartesian_impedance_example_controller/equilibrium_pose", PoseStamped, queue_size=50)
         self.action_client_open = actionlib.SimpleActionClient("/franka_gripper/move", MoveAction)
         self.action_client_close = actionlib.SimpleActionClient("/franka_gripper/grasp", GraspAction)
@@ -24,12 +27,14 @@ class arm_control():
         self.home  = [ 0.1000,  0.0, 1.000];
         self.blue  = [-0.4652, -0.5, 0.800];
         self.green = [-0.4652,  0.5, 0.800];
+        self.auxblue  = [-0.1000, -0.5, 0.800];
+        self.auxgreen = [-0.1000,  0.5, 0.800];
 
         destinations = [
             ['bBottle1' ,'blue' , 'bottle' ,'blue', -0.1700, -0.4600, 0.6135],
-            ['rCan2'    ,'red'  , 'can'    ,'blue', -0.0774, -0.6800, 0.6901],
-            ['gCan3'    ,'green', 'can'    ,'blue', -0.0771, -0.6800, 0.5734],
-            # ['gCan1'    ,'green', 'can'    ,'blue', -0.1067,  0.3634, 0.6900],
+            # ['rCan2'    ,'red'  , 'can'    ,'blue', -0.0774, -0.6800, 0.6901],
+            # ['gCan3'    ,'green', 'can'    ,'blue', -0.0771, -0.6800, 0.5734],
+            ['gCan1'    ,'green', 'can'    ,'blue', -0.1067,  0.3634, 0.6900],
             # ['rCan1'    ,'red'  , 'can'    ,'blue',  0.2950,  0.5031, 0.5857],
         ]
 
@@ -80,9 +85,18 @@ class arm_control():
             self.gripper_open()
             self.wait(self.time_to_wait)  
 
-        rospy.loginfo(f'Going to home.')
-        self.goto(self.home[0],self.home[1],self.home[2])       
-        self.wait(self.time_to_wait)  
+            if(d[3] == 'blue'):
+                rospy.loginfo(f'Going to aux blue.')
+                self.goto(self.auxblue[0],self.auxblue[1],self.auxblue[2])       
+                self.wait(self.time_to_wait)  
+            if(d[3] == 'green'):
+                rospy.loginfo(f'Going to aux green.')
+                self.goto(self.auxgreen[0],self.auxgreen[1],self.auxgreen[2])       
+                self.wait(self.time_to_wait)  
+
+            rospy.loginfo(f'Going to home.')
+            self.goto(self.home[0],self.home[1],self.home[2])       
+            self.wait(self.time_to_wait)  
 
     def goto(self, x, y, z):
 
@@ -114,10 +128,12 @@ class arm_control():
         self.action_client_close.wait_for_result()
 
     def wait(self, max_seconds):
-        start = time.time()
-        count = 0
-        while count < max_seconds:
-            count = time.time() - start            
+        rospy.sleep(max_seconds)
+
+        # start = time.time()
+        # count = 0
+        # while count < max_seconds:
+        #     count = time.time() - start            
 
 if __name__ == '__main__':
     rospy.init_node('arm_control', log_level=rospy.INFO)
